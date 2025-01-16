b5e8b6468a507d7966cdc77117a98417efc70330
João Kleber Linhalis <jklinhalis@gmail.com>
2025-01-16 15:29:04 -0300

    components for people

--- front_end/src/app/app.routes.ts
+++ front_end/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PeopleListComponent } from './components/people/people-list/people-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: PeopleListComponent,
+    }
+];
+
+
---
+++ front_end/src/app/components/people/people-form/people-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people-form',
+  imports: [],
+  templateUrl: './people-form.component.html',
+  styleUrl: './people-form.component.css'
+})
+export class PeopleFormComponent {
+
+}
