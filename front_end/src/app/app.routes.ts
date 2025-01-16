fd48e7b73d93408c6d6b0cbe79388c664ff4951e
João Kleber Linhalis <jklinhalis@gmail.com>
2025-01-16 15:35:25 -0300

    api config em people model

--- front_end/src/app/app.routes.ts
+++ front_end/src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { PeopleListComponent } from './components/people/people-list/people-list.component';
 
 export const routes: Routes = [
---
+++ front_end/src/app/models/people.ts
@@ -0,0 +1,9 @@
+export interface People {
+    id?: any,
+    first_name: string,
+    last_name: string,
+    state: string,
+    city: string,
+    email: string,
+    phone: string,
+}
